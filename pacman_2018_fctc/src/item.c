4f5e22af47125fd82f6a08ee9ab1bb44dd2aec50
JoonSeoPark <hjp1189@naver.com>
2018-05-16 03:08:41 -0700

    파일수정

--- pacman_2018_fctc/src/item.c
+++ pacman_2018_fctc/src/item.c
@@ -45,7 +45,12 @@ Item item_for_level(int level)
 			return Move_Slow;
 		else if(ran > 45 && ran <=60)
 			return Move_USD;
-
+		else if(ran > 60 && ran <=90)
+			return Misile;
+		else if(ran > 90 && ran <=120)
+			return Life;
+		else
+			return Prof;
 	}
 }
 
