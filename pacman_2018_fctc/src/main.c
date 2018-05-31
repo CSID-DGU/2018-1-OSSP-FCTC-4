4d804f404d459bf0588952ebaaca06edf97e7f4c
hyeonjong <guswhd9844@naver.com>
2018-05-31 19:55:58 +0900

    test

--- pacman_2018_fctc/src/main.c
+++ pacman_2018_fctc/src/main.c
@@ -242,10 +242,9 @@ static void internal_render(void)
 static void game_init(void)
 {
 	//Load the board here. We only need to do it once
-	load_board(&pacmanGame.board[0], &pacmanGame.pelletHolder[0], "maps/stage1_map");
+	load_board(&pacmanGame.board[0], &pacmanGame.pelletHolder[0], "maps/map1test");
 	load_board(&pacmanGame.board[1], &pacmanGame.pelletHolder[1], "maps/stage2_map");
 	load_board(&pacmanGame.board[2], &pacmanGame.pelletHolder[2], "maps/stage3_map");
-	load_board(&pacmanGame.board[2], &pacmanGame.pelletHolder[2], "maps/boss_map");
 	
 
 	//set to be in menu
